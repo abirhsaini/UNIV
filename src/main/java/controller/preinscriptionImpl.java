8fad6bd7bf2e65e105f1df118fa39f163d84500c
abirhsaini <abirhsaini@gmail.com>
2023-07-28 11:52:19 +0100

    page preinscription imprimer la preinscription

--- src/main/java/controller/preinscriptionImpl.java
+++ src/main/java/controller/preinscriptionImpl.java
@@ -1,17 +1,43 @@
 package controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import beans.etudiant;
 
 public class preinscriptionImpl {
 	
+
 	private Connection connection;
+	private HomeImpl homeImpl=new HomeImpl();
 	
 	public List<etudiant> getEtudiantsAdmis() throws ClassNotFoundException, SQLException {
 		
@@ -57,7 +83,280 @@ public class preinscriptionImpl {
 	    statement.setInt(2, IdEtudiant);
 	    int rSet= statement.executeUpdate();
 	    connection.close();
+	}
+	public etudiant chercherEtudiantCode(String codeEtudiant) throws ClassNotFoundException, SQLException {
+		connexion connexionInstance = new connexion(); 
+		connection = connexionInstance.connection();
+		PreparedStatement statement =connection.prepareStatement("select * from etudiant where codeEtudiant= ?");
+		
+		statement.setString(1,codeEtudiant);
+		ResultSet rSet= statement.executeQuery();
+		
+		if (rSet.next()) {
+			 etudiant etudiant=new etudiant();
+	    	 etudiant.setIdEtudiant(rSet.getInt("idEtudiant"));
+	    	 etudiant.setCodeEtudiant(rSet.getString("CodeEtudiant"));
+	    	 etudiant.setNom(rSet.getString("nom"));
+	    	 etudiant.setPrenom(rSet.getString("prenom"));
+	    	 etudiant.setCategorie(rSet.getString("categorie"));
+	    	 etudiant.setTypeDeBac(rSet.getString("typeDeBac"));
+	    	 etudiant.setNote(rSet.getDouble("note"));
+	    	 etudiant.setValider(rSet.getString("valider"));
+	    	 etudiant.setTel1(rSet.getString("tel1"));
+	    	 etudiant.setTel2(rSet.getString("tel2"));
+	    	 etudiant.setDimport(rSet.getString("Dimport"));
+	    	 etudiant.setStatut(rSet.getString("statut"));
+	    	 etudiant.setReg(rSet.getString("reg"));
+	    	 etudiant.setAdmission(rSet.getString("admission"));
+	    	 
+	    	 return etudiant;
+		}
+		return null;
+	}
+	public void importerEtudiant(FileInputStream inputStream) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
+			
+			try (
+	
+		             Workbook workbook = new XSSFWorkbook(inputStream)) {
+	
+		            Sheet sheet = workbook.getSheetAt(0);
+		            // Assuming your Excel data starts from the second row (index 1)
+		            for (Row row : sheet) {
+		                // Skip the header row (if any)
+		                if (row.getRowNum() == 0) {
+		                    continue;
+		                    
+		                }
+		                
+		             etudiant etudiant = new etudiant();
+		             String codeEtudiant = row.getCell(1).getStringCellValue();
+		             etudiant.setCodeEtudiant(codeEtudiant);
+	
+		             String nom = row.getCell(2).getStringCellValue();
+		             etudiant.setNom(nom);
+	
+		             String prenom = row.getCell(3).getStringCellValue();
+		             etudiant.setPrenom(prenom);
+	
+		             String categorie = row.getCell(4).getStringCellValue();
+		             etudiant.setCategorie(categorie);
+	
+		             String typeDeBac = row.getCell(5).getStringCellValue();
+		             etudiant.setTypeDeBac(typeDeBac);
+	
+		             double note = row.getCell(6).getNumericCellValue();
+		             etudiant.setNote(note);
+		             
+		      	     String tel1 = row.getCell(7).getStringCellValue();
+		             etudiant.setTel1(tel1);
+		             
+		             String tel2 = row.getCell(8).getStringCellValue();
+		             etudiant.setTel2(tel2);  
+		             
+		             etudiant.setValider("validé");
+		             
+		             String statut=row.getCell(9).getStringCellValue();
+		             etudiant.setStatut(statut);
+		             
+		             String reg =row.getCell(10).getStringCellValue();
+		             etudiant.setReg(reg);
+		             
+		             etudiant.setAdmission("admis");
+		             
+		             
+		             
+		             etudiant.setStatut("non saturé");
+		             etudiant.setDimport((new Date()).toString());
+		             etudiant.setReg("N.R");
+		             
+		             etudiant existEtudiant = chercherEtudiantCode(codeEtudiant);
+		             if (existEtudiant==null) {
+		             ajouterEtudiant(etudiant);
+		             }
+		            }
+		            
+		            System.out.println("Data imported successfully!");
+	
+		        } 
+		}
+
+	public void ajouterEtudiant(etudiant etudiant) throws ClassNotFoundException, SQLException {
+		connexion connexionInstance = new connexion(); 
+		connection = connexionInstance.connection();
+		PreparedStatement statement =connection.prepareStatement("INSERT INTO etudiant ( codeEtudiant, nom, prenom, categorie, typeDeBac, Note, tel1, tel2, valider, statut, Dimport, Reg, admission) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);");
+		
+			statement.setString(1, etudiant.getCodeEtudiant());
+		    statement.setString(2, etudiant.getNom());
+		    statement.setString(3, etudiant.getPrenom());
+		    statement.setString(4, etudiant.getCategorie());
+		    statement.setString(5, etudiant.getTypeDeBac());
+		    statement.setDouble(6, etudiant.getNote());
+		    statement.setString(7, etudiant.getTel1());
+		    statement.setString(8, etudiant.getTel2());
+		    statement.setString(9, etudiant.getValider());
+		    statement.setString(10, etudiant.getStatut());
+		    statement.setString(11, etudiant.getDimport());
+		    statement.setString(12, etudiant.getReg());
+		    statement.setString(13, etudiant.getAdmission());
+		    
+		statement.executeUpdate();
+		connection.close();
 		
 	}
 
+	public void modifierEtudiant(etudiant etudiant) throws ClassNotFoundException, SQLException {
+		connexion connexionInstance = new connexion(); 
+		connection = connexionInstance.connection();
+		
+		PreparedStatement statement =connection.prepareStatement("UPDATE etudiant SET codeEtudiant=?, nom=?, prenom=?, categorie=?, typeDeBac=?, Note=?, tel1=?, tel2=?, valider=?, statut=?, Dimport=?, Reg=?, admission=? WHERE idEtudiant=?");
+		
+		statement.setString(1, etudiant.getCodeEtudiant());
+	    statement.setString(2, etudiant.getNom());
+	    statement.setString(3, etudiant.getPrenom());
+	    statement.setString(4, etudiant.getCategorie());
+	    statement.setString(5, etudiant.getTypeDeBac());
+	    statement.setDouble(6, etudiant.getNote());
+	    statement.setString(7, etudiant.getTel1());
+	    statement.setString(8, etudiant.getTel2());
+	    statement.setString(9, etudiant.getValider());
+	    statement.setString(10, etudiant.getStatut());
+	    statement.setString(11, etudiant.getDimport());
+	    statement.setString(12, etudiant.getReg());
+	    statement.setInt(14, etudiant.getIdEtudiant());
+	    statement.setString(13, etudiant.getAdmission());
+	    System.out.println(statement);    
+	statement.executeUpdate();
+		
+	}
+
+	public etudiant chercherEtudiant(int idEtudiant ) throws ClassNotFoundException, SQLException {
+		connexion connexionInstance = new connexion(); 
+		connection = connexionInstance.connection();
+		PreparedStatement statement =connection.prepareStatement("select * from etudiant where idEtudiant= ?");
+		
+		statement.setInt(1,idEtudiant );
+		ResultSet rSet= statement.executeQuery();
+		
+		if (rSet.next()) {
+			 etudiant etudiant=new etudiant();
+	    	 etudiant.setIdEtudiant(rSet.getInt("idEtudiant"));
+	    	 etudiant.setCodeEtudiant(rSet.getString("CodeEtudiant"));
+	    	 etudiant.setNom(rSet.getString("nom"));
+	    	 etudiant.setPrenom(rSet.getString("prenom"));
+	    	 etudiant.setCategorie(rSet.getString("categorie"));
+	    	 etudiant.setTypeDeBac(rSet.getString("typeDeBac"));
+	    	 etudiant.setNote(rSet.getDouble("note"));
+	    	 etudiant.setValider(rSet.getString("valider"));
+	    	 etudiant.setTel1(rSet.getString("tel1"));
+	    	 etudiant.setTel2(rSet.getString("tel2"));
+	    	 etudiant.setDimport(rSet.getString("Dimport"));
+	    	 etudiant.setStatut(rSet.getString("statut"));
+	    	 etudiant.setReg(rSet.getString("reg"));
+	    	 
+	    	 return etudiant;
+		}
+		return null;
+	}
+	public byte[] createPDFPreinscription(etudiant etudiant) throws IOException {
+			LocalDate todaysDate = LocalDate.now();
+		    String fileName = "C:\\Users\\pc\\eclipse-workspace\\UNIV\\src\\main\\webapp\\images\\preinscription.pdf";
+	        String content = "This is an example PDF text that will be written into the PDF file.";
+
+	        try {
+	        	 // Create a new Document
+	            Document document = new Document();
+
+	            // Create a ByteArrayOutputStream to write the PDF content
+	            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+	            // Create a PdfWriter to write the document to the ByteArrayOutputStream
+	            PdfWriter.getInstance(document, baos);
+
+
+	            
+	            // Open the document to write content
+	            document.open();
+	            String FCZPath = "C:\\Users\\pc\\eclipse-workspace\\UNIV\\src\\main\\webapp\\images/logo-off-1.png";
+	            Image imageFCZ = Image.getInstance(FCZPath);
+	            imageFCZ.scaleToFit(50, 50);
+	            
+	            String abuPath = "C:\\Users\\pc\\eclipse-workspace\\UNIV\\src\\main\\webapp\\images/abulkassis.png";
+	            Image imageabu = Image.getInstance(abuPath);
+	            imageabu.scaleToFit(100, 50);
+	            imageabu.setAbsolutePosition(PageSize.A4.getWidth() - imageabu.getScaledWidth() - 30, PageSize.A4.getHeight() - imageabu.getScaledHeight() - 30);
+	            
+	            String cachetPath="C:\\Users\\pc\\eclipse-workspace\\UNIV\\src\\main\\webapp\\images/cachet.png";
+	            Image imagecachet=Image.getInstance(cachetPath);
+	            imagecachet.scaleToFit(100, 100);
+	            imagecachet.setAlignment(Element.ALIGN_RIGHT);
+	            
+	            Font font = new Font(Font.FontFamily.TIMES_ROMAN, 20, Font.BOLD,BaseColor.BLACK);
+	            Font fontbody = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL,BaseColor.BLACK);
+	            
+	            Chunk chunkbody = new Chunk("Nous, soussigné Université internationale ABULCASSIS des sciences et santé , après examen du dossier présenté, donnons acte à ",fontbody);
+	            Font fontEtudiant=new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD,BaseColor.BLACK);
+				Chunk chunketudiantChunk =new Chunk( etudiant.getNom() + " " + etudiant.getPrenom() , fontEtudiant);
+	            Chunk chunkBody1 = new Chunk(" sa préinscription en Université internationale ABULCASSIS des sciences et santé au titre de l'année academique 2022 - 2023 .",fontbody);
+	            Chunk chunkbody2=new Chunk("L'inscription ne sera definitive qu'apres l'admission dans le concour.",fontbody);
+	            @SuppressWarnings("deprecation")
+				Chunk chunkbody3 =new Chunk ("Fait à Rabat, le "+ todaysDate, fontbody);
+	            Chunk chunkfooter =new Chunk ("Direction" , fontEtudiant);
+	            Chunk chunk = new Chunk("Attestation de la préinscription ", font);
+	            
+	            Paragraph objectParagraph = new Paragraph();
+	            Paragraph bodyParagraph = new Paragraph();
+	            Paragraph bodyParagraph1 = new Paragraph();
+	            Paragraph bodyParagraph2 = new Paragraph();
+	            Paragraph footerParagraph =new Paragraph();
+	            
+	            objectParagraph.add(chunk);
+	            objectParagraph.setAlignment(Element.ALIGN_CENTER);
+	            objectParagraph.setSpacingBefore(150);
+	            objectParagraph.setSpacingAfter(30);
+	            
+	            bodyParagraph.add(chunkbody);
+	            bodyParagraph.add(chunketudiantChunk);
+	            bodyParagraph.add(chunkBody1);
+	            footerParagraph.add(chunkfooter);
+	            
+	            bodyParagraph.setSpacingAfter(20);
+	            bodyParagraph1.add(chunkbody2);
+	            bodyParagraph1.setSpacingAfter(20);
+	            bodyParagraph2.add(chunkbody3);
+	            bodyParagraph2.setSpacingAfter(100);
+	            footerParagraph.setAlignment(Element.ALIGN_RIGHT);
+	            footerParagraph.setSpacingAfter(40);
+	            
+	            
+	            
+	            
+	            // Add content to the document
+	            document.add(imageFCZ);
+	            document.add(imageabu);
+	            document.add(objectParagraph);
+	            document.add(bodyParagraph);
+	            document.add(bodyParagraph1);
+	            document.add(bodyParagraph2);
+	            document.add(footerParagraph);
+	            document.add(imagecachet);
+	            document.addTitle("attestation de la preinscription");
+	            
+	            // Close the document
+	            document.close();
+
+	            // Get the PDF content as a byte array
+	            byte[] pdfContent = baos.toByteArray();
+	            baos.close();
+
+	            return pdfContent;
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            return null;
+	        }
+			
+	    }
+	public void imprimmerPreinscription(etudiant etudiant) {
+		
+		
+	}
 }
--- src/main/java/servelet/Preinscription.java
+++ src/main/java/servelet/Preinscription.java
@@ -1,23 +1,29 @@
 package servelet;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import beans.etudiant;
 import controller.HomeImpl;
 import controller.preinscriptionImpl;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
 
 /**
  * Servlet implementation class Preinscription
  */
+@MultipartConfig
 public class Preinscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	preinscriptionImpl preinscriptionImpl =new controller.preinscriptionImpl();
@@ -42,7 +48,7 @@ public class Preinscription extends HttpServlet {
 		HttpSession session = request.getSession();
 		try {
 			List<etudiant> etudiants = preinscriptionImpl.getEtudiantsAdmis();
-			System.out.println(etudiants);
+			
 			session.setAttribute("etudiants", etudiants);
 		} catch (ClassNotFoundException | SQLException e1) {
 			// TODO Auto-generated catch block
@@ -82,9 +88,143 @@ public class Preinscription extends HttpServlet {
 		        response.sendRedirect(request.getContextPath() + "/Preinscription");
 		        return; // Optional, but it's good practice to stop further processing of the request after the redirect.
 		    }
+		    if (action.equals("importer")) {
+		        Part filePart = request.getPart("file");
+		        
+		        FileInputStream fileInputStream =(FileInputStream) filePart.getInputStream();
+		        System.out.print(filePart.getName());
+		        try {
+					preinscriptionImpl.importerEtudiant(fileInputStream);
+					response.sendRedirect(request.getContextPath() + "/Preinscription");
+			        return;
+				} catch (ClassNotFoundException | SQLException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        
+		        response.setStatus(HttpServletResponse.SC_OK);
+		    }
+		    if(action.equals("ajouter")) {
+		    	System.out.println("ajouter entrer");
+		    	etudiant etudiant =new etudiant();
+		    	Date date= new Date();
+		    	String etudiantCode= request.getParameter("studentCode");
+		    	String etudiantnom= request.getParameter("studentName");
+		    	String etudiantprenom= request.getParameter("studentPrenom");
+		    	String studentCategorie= request.getParameter("studentCategorie");
+		    	String studentTypeDeBac= request.getParameter("studentTypeDeBac");
+		    	String studentNote= request.getParameter("studentNote");
+		    	String studentTel1= request.getParameter("studentTel1");
+		    	String studentTel2= request.getParameter("studentTel2");
+		    	String etudiantadmission= request.getParameter("admission");
+		    	String etudiantsatuer= request.getParameter("satuer");
+		    	String etudiantReglement= request.getParameter("reglement");
+		    	
+		    	etudiant.setCategorie(studentCategorie);
+		    	etudiant.setCodeEtudiant(etudiantCode);
+		    	etudiant.setDimport(date.toString());
+		    	etudiant.setNom(etudiantnom);
+		    	etudiant.setNote(Double.parseDouble(studentNote));
+		    	etudiant.setPrenom(etudiantprenom);
+		    	etudiant.setReg(etudiantReglement);
+		    	etudiant.setStatut(etudiantsatuer);
+		    	etudiant.setTel1(studentTel1);
+		    	etudiant.setAdmission(etudiantadmission);
+		    	etudiant.setTel2(studentTel2);
+		    	etudiant.setTypeDeBac(studentTypeDeBac);
+		    	etudiant.setValider("validé");
+		    	
+		    	try {
+					preinscriptionImpl.ajouterEtudiant(etudiant);
+					response.sendRedirect(request.getContextPath() + "/Preinscription");
+					return;
+				} catch (ClassNotFoundException | SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	
+		    	
+		    	
+		    }
+		    if (action.equals("modifier-phase2")) {
+		    	String[] selectedIdsArray = request.getParameterValues("selectedIdsInput");
+		 	    System.out.println("selectedIdsArray: " + Arrays.toString(selectedIdsArray));
+		 	    int numberOfSelectedStudents = selectedIdsArray.length;
+		 	    request.setAttribute("numberOfSelectedStudents", numberOfSelectedStudents);
+		 	   List<String> selectedIds = Arrays.asList(selectedIdsArray);
+		    	try {
+					etudiant etudiant=preinscriptionImpl.chercherEtudiant(Integer.parseInt(selectedIds.get(0)));
+					String etudiantCode= request.getParameter("studentCode1");
+			    	String etudiantnom= request.getParameter("studentName1");
+			    	String etudiantprenom= request.getParameter("studentPrenom1");
+			    	String studentCategorie= request.getParameter("categorie1");
+			    	System.out.println(studentCategorie);
+			    	String studentTypeDeBac= request.getParameter("typeDeBac1");
+			    	String studentNote= request.getParameter("studentNote1");
+			    	String studentTel1= request.getParameter("studentTel11");
+			    	String studentTel2= request.getParameter("studentTel21");
+			    	String etudiantadmission= request.getParameter("admission1");
+			    	String etudiantsatuer= request.getParameter("statut1");
+			    	String etudiantReglement= request.getParameter("reglement1");
+			    	etudiant.setCategorie(studentCategorie);
+			    	etudiant.setCodeEtudiant(etudiantCode);
+			    	etudiant.setNom(etudiantnom);
+			    	etudiant.setStatut(etudiantsatuer);
+			    	etudiant.setNote(Double.parseDouble(studentNote));
+			    	etudiant.setPrenom(etudiantprenom);
+			    	etudiant.setReg(etudiantReglement);
+			    	etudiant.setStatut(etudiantsatuer);
+			    	etudiant.setTel1(studentTel1);
+			    	etudiant.setTel2(studentTel2);
+			    	etudiant.setTypeDeBac(studentTypeDeBac);
+			    	etudiant.setAdmission(etudiantadmission);
+			    	etudiant.setValider("validé");
+			    	preinscriptionImpl.modifierEtudiant(etudiant);
+			    	response.sendRedirect(request.getContextPath() + "/Preinscription");
+			        return;
+				} catch (NumberFormatException | ClassNotFoundException | SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    
+		    }
+		   if (action.equals("imprimer")) {
+			   System.out.println("deja fait");
+			    String[] selectedIdsArray = request.getParameterValues("selectedIdsInput");
+		 	    System.out.println("selectedIdsArray: " + Arrays.toString(selectedIdsArray));
+		 	    int numberOfSelectedStudents = selectedIdsArray.length;
+		 	    request.setAttribute("numberOfSelectedStudents", numberOfSelectedStudents);
+		 	    List<String> selectedIds = Arrays.asList(selectedIdsArray);
+		 	    String[] ids = selectedIdsArray[0].split(",");
+		 	    List<String> selectedIdsList = new ArrayList<>();
+				selectedIdsList.addAll(Arrays.asList(ids));
+				if (selectedIdsList.size()==1) {
+					
+				
+		 	   byte[] pdfContent = null;
+			try {
+				pdfContent = (byte[]) preinscriptionImpl.createPDFPreinscription(preinscriptionImpl.chercherEtudiant(Integer.parseInt(selectedIds.get(0))));
+			} catch (NumberFormatException | ClassNotFoundException | IOException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		        // Set response headers
+		        response.setContentType("application/pdf");
+		        response.setContentLength(pdfContent.length);
+		        String headerKey = "Content-Disposition";
+		        String headerValue = "attachment; filename=preinscription.pdf";
+		        response.setHeader(headerKey, headerValue);
+
+		        // Write the PDF content to the response output stream
+		        response.getOutputStream().write(pdfContent);
+		        response.getOutputStream().flush();
+		   }
+	    }
+	    }
 		doGet(request, response);
 	}
-	}
+	
 
 	}
 
